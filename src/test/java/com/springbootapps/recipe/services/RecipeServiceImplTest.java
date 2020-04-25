afdd5130eee283d2c9b7e2d6a97cfbb89365739c
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-04-25 09:19:10 +0530

    recipeservice, closes #14

--- src/test/java/com/springbootapps/recipe/services/RecipeServiceImplTest.java
+++ src/test/java/com/springbootapps/recipe/services/RecipeServiceImplTest.java
@@ -1,11 +1,18 @@
 package com.springbootapps.recipe.services;
 
+import com.springbootapps.recipe.domain.Recipe;
 import com.springbootapps.recipe.repositories.RecipeRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 class RecipeServiceImplTest {
 
@@ -27,7 +34,10 @@ class RecipeServiceImplTest {
 
     @Test
     void getRecipes() {
-        //todo:mock - when and verify
-        recipeService.getRecipes();
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        when(recipeRepository.findAll()).thenReturn(recipes);
+        assertEquals(recipes.size(), recipeService.getRecipes().size());
+        verify(recipeRepository, times(1)).findAll();
     }
 }
