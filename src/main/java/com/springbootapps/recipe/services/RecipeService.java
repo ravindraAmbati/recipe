f8f907e08490e595eb0910e7a542816c4e023ceb
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-04-26 20:18:28 +0530

    delete recipe, closes #25

--- src/main/java/com/springbootapps/recipe/services/RecipeService.java
+++ src/main/java/com/springbootapps/recipe/services/RecipeService.java
@@ -18,4 +18,6 @@ public interface RecipeService {
     RecipeCommand findCommandById(Long l);
 
     RecipeCommand saveRecipeCommand(RecipeCommand command);
+
+    void deleteById(Long idToDelete);
 }
