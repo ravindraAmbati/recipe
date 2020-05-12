26d465db09a89d072100c5398214e4ec66c8d84a
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-05-12 19:41:57 +0530

    add html 404 error, closes #34

--- src/main/java/com/springbootapps/recipe/controllers/RecipeController.java
+++ src/main/java/com/springbootapps/recipe/controllers/RecipeController.java
@@ -1,14 +1,14 @@
 package com.springbootapps.recipe.controllers;
 
 import com.springbootapps.recipe.commands.RecipeCommand;
+import com.springbootapps.recipe.exceptions.NotFoundException;
 import com.springbootapps.recipe.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by jt on 6/19/17.
@@ -57,5 +57,14 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        log.error("Not Found Exception");
+        return modelAndView;
     }
 }
